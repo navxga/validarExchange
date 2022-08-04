57e3a391c75823227f01488b31ab39e537257758
nav <lyannavega@gmail.com>
2022-08-04 15:50:20 -0300

    melhorias na interface

--- vE.py
+++ vE.py
@@ -7,22 +7,28 @@ import json
 p = pyautogui
 def openExchange():
     p.hotkey('win', 'r')
-    p.write('cmd')
-    # p.write('C:\\Users\\lnavega\\Projetos\\integra\\src\\Integra.ExchangeService\\bin\\Debug\\netcoreapp3.1\\Integra.ExchangeService.exe')
+    # p.write('cmd')
+    p.write('C:\\Users\\lnavega\\Projetos\\integra\\src\\Integra.ExchangeService\\bin\\Debug\\netcoreapp3.1\\Integra.ExchangeService.exe')
     p.press('enter')
 def execute():
     p.write('execute ')
     p.write(id)
     p.press('enter')
 
+
+
+
+
 # # API
 # endpoint = 'https://api-integra-private.impacta.adv.br/api/private/v1/cliente/integracao/pendente/execucao'
 
 # requisicao = requests.get(endpoint)
 
-# print('Start')
 # print(requisicao)
-# print('End')
+# print(requisicao.json())
+
+
+
 
 
 # Entrada do Json
@@ -33,19 +39,23 @@ with open("listaId.json", encoding = "UTF-8") as jsn:
 openExchange()
 sleep(1)
 
-executados = 1
+executados = 0
+total = 0
 
 for i in ids:
+    total += 1
     if i['Cliente'] != 'DAYCOVAL':
-        print(executados, i['Cliente'], end = ' ')
-
+        print(total, i['Cliente'], end = ' ')
         id = i['Id']
         execute()
         print('executado.')
         executados += 1
-        sleep(15)
+        sleep(10)
     else:
-        print(executados, i['Cliente'])
+        print(total, i['Cliente'])
+        sleep(0.5)
 
+print('='*20)
 print('Fim da Execução!')
-print(f'{executados} Ids foram executados.')
+print(f'Total de Reqs: {total}\n{executados} Ids foram executados.')
+print('='*20)
