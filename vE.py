283f2242db2f331b787112dd04005ee47f60842a
navxga <lyannavega@gmail.com>
2022-08-03 22:58:18 -0300

    inicio dos testes com API

--- vE.py
+++ vE.py
@@ -1,17 +1,29 @@
 from time import sleep
 import pyautogui
+# import requests
 import json
 
 # Declarações
 p = pyautogui
 def openExchange():
     p.hotkey('win', 'r')
-    p.write('C:\\Users\\lnavega\\Projetos\\integra\\src\\Integra.ExchangeService\\bin\\Debug\\netcoreapp3.1\\Integra.ExchangeService.exe')
+    p.write('cmd')
+    # p.write('C:\\Users\\lnavega\\Projetos\\integra\\src\\Integra.ExchangeService\\bin\\Debug\\netcoreapp3.1\\Integra.ExchangeService.exe')
     p.press('enter')
 def execute():
     p.write('execute ')
     p.write(id)
     p.press('enter')
+
+# # API
+# endpoint = 'https://api-integra-private.impacta.adv.br/api/private/v1/cliente/integracao/pendente/execucao'
+
+# requisicao = requests.get(endpoint)
+
+# print('Start')
+# print(requisicao)
+# print('End')
+
 
 # Entrada do Json
 with open("listaId.json", encoding = "UTF-8") as jsn:
